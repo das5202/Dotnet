a3dd84ac9583317cd2ff1c258b009695f5caaa95
devikadasari <devikarani1182001@gmail.com>
2023-08-09 21:10:20 +0530

    firstassignment

---
+++ Assignment1/Controllers/DummyController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class DummyController : ControllerBase
+    {
+        private List<string> dev = new List<string>()
+{
+"number1",
+"number2",
+"number3"
+};
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            return Ok(dev);
+        }
+        [HttpGet("getbyId")]
+        public IActionResult GetById(int id)
+        {
+            if (id >= 0 && id < dev.Count)
+            {
+                return Ok(dev[id]);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost("create")]
+        public IActionResult create([FromBody] string newdev)
+        {
+            dev.Add(newdev);
+            return Created("", newdev);
+        }
+        [HttpPut("updateById")]
+        public IActionResult Update(int id, [FromBody] string updev)
+        {
+            if (id >= 0 && id < dev.Count)
+            {
+                dev[id] = updev;
+                return Ok(updev);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("deletebyid")]
+        public IActionResult DeleteById(int id)
+        {
+            if (id >= 0 && id < dev.Count)
+            {
+                dev.RemoveAt(id);
+                return Ok(dev);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
+
+
+
---
+++ Assignment1/Controllers/TestController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Assignment1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static List<string> A = new List<string>()
+        {
+        "EmployeA","EmployeB","EmployeeC"};
+        //Get: api/Test
+        [HttpGet("all")]
+        public IActionResult Get()
+        {
+           
+            return Ok(A);
+        }
+        //Get: api/Test/1
+        [HttpGet("allbyId")]
+        public IActionResult GetById(int  id)
+        {
+            if (id >= 0 && id < A.Count) {
+                return Ok(A[id]);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        //Post: api/Test
+        [HttpPost("create")]
+        public IActionResult Post([FromBody]string neA)
+        {
+            A.Add(neA);
+            return Created("", neA);
+        }
+        //Put: api/Test/1
+        [HttpPut("updatebyid")]
+        public IActionResult Update(int id, [FromBody] string uA)
+        {
+            if (id >= 0 && id < A.Count)
+            {
+                A[id] = uA;
+                return Ok(A);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        //Delete: api/Test/1
+        [HttpDelete("Deletebyid")]
+        public IActionResult Delete(int id) 
+        {
+            if (id >= 0 && id < A.Count)
+            {
+                A.RemoveAt(id);
+                return Ok(A);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+       
+        }
+    }
+
